a9656c12ac6166f7a788962732be65581c70b93f
taes-k <cobura12@gmail.com>
2021-10-24 15:10:36 +0900

    project init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "io.github.taesk"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}

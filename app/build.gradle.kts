96d96d0e9436dc0ff1b77a82e2a23286a56a8b2e
taes-k <cobura12@gmail.com>
2021-12-10 22:29:11 +0900

    wip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
     implementation(project(":annotation-processor"))
     annotationProcessor(project(":annotation-processor"))
 
-    testImplementation("org.mockito:mockito-all:1.10.19")
+    implementation("org.mockito:mockito-all:1.10.19")
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("org.apache.commons:commons-collections4:4.4")
 
@@ -13,23 +13,4 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-}
-
-val generatedSourcePath = "$buildDir/generated/sources/annotationProcessor/java/test"
-val generatedOutputDir = file(generatedSourcePath)
-
-tasks.withType<JavaCompile> {
-    doFirst {
-        generatedOutputDir.exists() || generatedOutputDir.mkdirs()
-        options.compilerArgs = listOf("-s", generatedSourcePath)
-    }
-//    options.annotationProcessorGeneratedSourcesDirectory = generatedOutputDir
-//    outputs.dir(generatedSourcePath)
-}
-
-sourceSets.main {
-    java.exclude(generatedSourcePath + "/**")
-}
-sourceSets.test {
-    java.srcDirs(generatedOutputDir)
 }
